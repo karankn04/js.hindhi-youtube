1e65ffe512547a3948a75a7bca1fea71519f4ff0
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-11 10:14:55 +0000

    add variable declarations and comments for data types

--- .devcontainer/01_basics/02_datatype.js
+++ .devcontainer/01_basics/02_datatype.js
@@ -6,3 +6,15 @@ console.log(3+3)
 
 console.log("hitesh");
 
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
