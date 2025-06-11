5ef43e00678d7b396100b2bbc6dca81388072709
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-11 10:07:52 +0000

    chnages

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId =14455
+let accounemail="hitesh@google.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+
+accountId = 2
+
+console.log(accountId);
---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,8 @@
+"use strict";// treat all js code as newer version
+
+//alert("hello")// we are using node js not brows
+
+console.log(3+3)
+
+console.log("hitesh");
+
