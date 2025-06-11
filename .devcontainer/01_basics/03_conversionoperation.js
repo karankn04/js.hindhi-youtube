c9412ff464ab49e16318ae981628d291195a6fcc
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-11 10:37:23 +0000

    add initial conversion operation examples

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,14 @@
+let score =true
+
+/*console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+*/
+
+/*let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+*/
