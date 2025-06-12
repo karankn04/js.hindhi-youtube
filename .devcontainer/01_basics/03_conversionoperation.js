954096effd1e5406aa3281473571d22c670f3ae6
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-12 07:02:54 +0000

    refactor: comment out unused code and clarify variable increment examples

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -12,10 +12,20 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 console.log(typeof booleanIsLoggedIn);
 */
-let value = 3
-let negativeValue = -value; 
-console.log(negativeValue); // Output: -3  
-console.log("1"+2);
-console.log(1+2+"1"); // Output: 31
-console.log("1"+2+3); // Output: 123
-console.log(1+2+"3"); // Output: 33
+// let value = 3
+// let negativeValue = -value; 
+// console.log(negativeValue); // Output: -3  
+// console.log("1"+2);
+// console.log(1+2+"1"); // Output: 31
+// console.log("1"+2+3); // Output: 123
+// console.log(1+2+"3"); // Output: 33
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
