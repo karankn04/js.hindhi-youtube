1f5912568139014fb4fbad2a05c5c50c65c1fd25
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-12 06:40:21 +0000

    add additional examples for number and string conversion operations

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -12,3 +12,10 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 console.log(typeof booleanIsLoggedIn);
 */
+let value = 3
+let negativeValue = -value; 
+console.log(negativeValue); // Output: -3  
+console.log("1"+2);
+console.log(1+2+"1"); // Output: 31
+console.log("1"+2+3); // Output: 123
+console.log(1+2+"3"); // Output: 33
