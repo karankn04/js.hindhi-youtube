2b498d042b32d483db49003cfb0c1621ade07a39
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-14 16:40:27 +0000

    add examples for comparison operations and data types

---
+++ .devcontainer/01_basics/04_camparison.js
@@ -0,0 +1,19 @@
+// console.log("2">1);
+// console.log("2"<1);
+
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null<0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined<0);
+
+//===  strictly check the value and type
+
+console.log("1"===1);
+
+
+
---
+++ .devcontainer/01_basics/05_datatype_summary.js
@@ -0,0 +1,34 @@
+//primitive
+
+//7types ; string,number,boolean,null,undefined,symbol,bigint
+
+const score = 100; //number
+const scorevalue = 100.3
+
+const isActive = true; //boolean
+const outsidetemp = null; //null
+let useremail; //undefined
+
+const id = Symbol('123'); //symbol
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+ //false, symbols are unique
+
+//regrence(non primitive)
+//object,array,function
+
+
+
+
+
+
+//++++++++++++++++++++++++
+
+//stack(primitive) vs heap(non primitive)
+
+let myname = "karan"
+
+let anothername = bittu
+
+console
