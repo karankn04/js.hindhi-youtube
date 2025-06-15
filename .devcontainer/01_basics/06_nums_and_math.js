2d0a103192d2858186b0f1eec19581f18ada0670
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-15 16:53:38 +0000

    refactor: comment out unused console log statements for clarity

--- .devcontainer/01_basics/06_nums_and_math.js
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -1,10 +1,31 @@
 const score = 400
 
 const balance = new Number(1000.50)
-console.log(balance)
+// console.log(balance)
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(3));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
 
 const othernum = 123.456
-console.log(othernum.toPrecision());
+// console.log(othernum.toPrecision(4));
+
+const hundread =100000
+// console.log(hundread.toLocaleString('en-IN'));
+
+
+
+// +++++++ Math +++++++
+
+// console.log(Math);
+// console.log(Math.abs(-42)); // absolute value
+// console.log(Math.round(4.7)); // round to nearest integer
+// console.log(Math.ceil(4.1)); // round up
+// console.log(Math.floor(4.9)); // round down
+// console.log(Math.max(1, 2, 3, 4, 5)); // maximum value
+// console.log(Math.min(1, 2, 3, 4, 5)); // minimum value
+console.log((Math.random()*10)+1); // random number between 0 and 1
+
+const min = 10
+const max = 20  
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // random number between min and max
