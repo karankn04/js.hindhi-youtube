235b02f41cd7d4381011cf4737027860fe55ac12
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-17 10:21:08 +0000

    add: implement singleton object with greeting method

---
+++ devcontainer/02_basics/03_objects.js
@@ -0,0 +1,19 @@
+//singleton object
+
+//OBJECT LITERALS
+// Object.create()
+const jsuser={
+    name:"karan",
+    age :21,
+    location: "jaipur",
+    email : "karan@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday","saturday"]
+}
+// console.log(jsuser.email)
+// console.log(jsuser["email"])
+
+jsuser.greeting = function(){
+    console.log("Hello, " + this.name)
+};
+jsuser.greeting()
