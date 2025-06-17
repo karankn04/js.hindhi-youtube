435f49c3c0f0d25f615351740c5e5388e44ddcd2
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-17 06:36:55 +0000

    add: implement various JavaScript examples including variables, data types, arrays, and math operations

---
+++ devcontainer/02_basics/02_array.js
@@ -0,0 +1,11 @@
+const marvel_heroes =["thor", "iron", "hulk", "spiderman", "captain america"];
+const dc_heroes=["batman", "superman", "flash", "green lantern", "aquaman"];
+
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes[3][1]); // ["thor", "iron", "hulk", "spiderman", "captain america", Array(5)]
+
+// const allhero=marvel_heroes.concat(dc_heroes);
+// console.log(allhero); // ["thor", "iron", "hulk",
+const allhero2=[...marvel_heroes, ...dc_heroes];
+
+console.log(allhero2); // ["thor", "iron", "hulk", "spiderman", "captain america", "batman", "superman
