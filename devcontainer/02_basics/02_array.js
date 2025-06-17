5d9d278db26992ebedd5bd5b49481cdcf5abbe9b
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-06-17 06:43:21 +0000

    refactor: clean up console log statements and add array method examples

--- devcontainer/02_basics/02_array.js
+++ devcontainer/02_basics/02_array.js
@@ -8,4 +8,11 @@ const dc_heroes=["batman", "superman", "flash", "green lantern", "aquaman"];
 // console.log(allhero); // ["thor", "iron", "hulk",
 const allhero2=[...marvel_heroes, ...dc_heroes];
 
-console.log(allhero2); // ["thor", "iron", "hulk", "spiderman", "captain america", "batman", "superman
+// console.log(allhero2); // ["thor", "iron", "hulk", "spiderman", "captain america", "batman", "superman
+
+
+console.log(Array.isArray("karan")); // false
+console.log(Array.from("karan"));
+ // [ 'k', 'a', 'r', 'a', 'n' ]
+
+ 
