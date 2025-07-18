cff6841cd90e756eaaf4ab47f283594140a0c3dc
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-07-18 11:06:27 +0000

    add: implement immediately invoked function expression (IIFE) example

---
+++ devcontainer/03_basics/04_iife.js
@@ -0,0 +1,8 @@
+// immediately invoke functuon expression
+
+(function(){
+    console.log("Hello, this is an IIFE!");
+})();
+// This will print "Hello, this is an IIFE!" immediately when the script is run
+
+((name))
