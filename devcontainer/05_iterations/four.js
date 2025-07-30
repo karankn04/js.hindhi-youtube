e61c28d2ccb6b38972d3e5b260e161ee141ea7ff
karankn04 <167538391+karankn04@users.noreply.github.com>
2025-07-30 09:11:40 +0000

    add: create initial structure for object and array iteration examples

---
+++ devcontainer/05_iterations/four.js
@@ -0,0 +1,19 @@
+// const myObject = {
+//     js: 'javascript',
+//     py: 'python',
+//     rb: 'ruby',
+//     swift:"swift by apple",
+
+// }
+
+// for (const key in myObject) {
+//     // console.log(`${key} shourtcut is ${myObject[key]}`);
+    
+
+// }
+    
+// const programming =["js","py","rb","swift"]
+
+// for (const key in programming) {
+//     console.log(programming[key])
+    
